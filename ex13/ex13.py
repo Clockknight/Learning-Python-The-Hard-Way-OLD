e3df3e5be536166a24231a2d4e215b0df703ab20
Clockknight <34818025+Clockknight@users.noreply.github.com>
2018-11-05 18:32:16 -0800

    Create ex13.py

---
+++ ex13/ex13.py
@@ -0,0 +1,9 @@
+from sys import argv
+#See WYSS for full explanation
+#Tyler's note: Unrunnable (to my knowledge) in repl.it, you have to export it into a desktop and run it from PowerShell.
+script, first, second, third = argv
+
+print("The script is called:", script)
+print("Your first variable is:", first)
+print("Your second variable is:", second)
+print("Your third variable is:", third)
