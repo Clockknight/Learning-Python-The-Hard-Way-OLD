bd07453a93e7851d9d299ebf86901d0d8487078a
Clockknight <34818025+Clockknight@users.noreply.github.com>
2018-11-05 18:25:40 -0800

    Create ex2.py

---
+++ ex2/ex2.py
@@ -0,0 +1,8 @@
+#Comment
+#Ignored by compilers
+print('I could have code like this.')#and ignore the comment later
+
+#use comments to "disable" code
+#print("like this.")
+
+print ("Not like this.")
